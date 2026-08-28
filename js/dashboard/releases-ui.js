32a110c513f3f86b84e9c19fd3367e21c3198768
Guglielmo Piersanti <guglielmopiersanti@MacBook-Air-di-Guglielmo.local>
2026-08-28 15:32:46 +0200

    fix(releases): corretto URL anteprima Hub Admin su index.html

--- js/dashboard/releases-ui.js
+++ js/dashboard/releases-ui.js
@@ -8,8 +8,8 @@ const ReleasesUI = {
             id: 'hub_admin',
             name: 'Hub Dashboard Admin',
             repo: 'prof-memmo-gestione-siti',
-            liveUrl: 'https://prof-memmo.github.io/prof-memmo-gestione-siti/portal.html',
-            previewUrl: 'https://prof-memmo.github.io/prof-memmo-gestione-siti/preview/portal.html',
+            liveUrl: 'https://prof-memmo.github.io/prof-memmo-gestione-siti/index.html',
+            previewUrl: 'https://prof-memmo.github.io/prof-memmo-gestione-siti/preview/index.html',
             icon: 'fa-shield-halved',
             color: '#6366f1',
             description: 'Console di amministrazione centrale e strumenti di gestione.'
